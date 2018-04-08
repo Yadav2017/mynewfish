ce8ec8ce6d357c5b36a84e3eb2bdaa0ce64aa09a
Abhi <7hacx9@gmail.com>
2018-04-08 19:34:43 +0530

    map commit
    map commit

---
+++ MapsPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Maps
+{
+    public partial class MapsPage : ContentPage
+    {
+        public MapsPage()
+        {
+            InitializeComponent();
+            var mp = new CustomMap();
+
+            var pin = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(37.79752, -122.40183),
+                Label = "YMCA",
+                Address = "394 Pacific Ave, San Francisco CA",
+                Id = "Xamarin",
+                Url = "http://ymca.org",
+                streetview = "Abhishek"
+            };
+            var pin2 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(37.79500, -122.40000),
+                Label = "YMCA San Francisco Office",
+                Address = "394 Pacific Ave, San Francisco CA",
+                Id = "Xamarin",
+                Url = "https://www.google.com/maps/@33.2249104,-117.2909954,3a,90y,-4.81h,88.84t/data=!3m6!1e1!3m4!1sAF1QipNJoAGctctHWEOcli2MR1SkS2dGySR7OxaHMlmT!2e10!7i13312!8i6656",
+                streetview = "my home"
+            };
+
+            mp.CustomPins = new List<CustomPin> { pin, pin2 };
+            foreach (var item in mp.CustomPins)
+            {
+                mp.Pins.Add(item);
+
+            }
+
+
+
+            mp.MoveToRegion(MapSpan.FromCenterAndRadius(
+              new Position(37.79752, -122.40183), Distance.FromMiles(1.0)));
+
+            Content = mp;
+        }
+    }
+
+    public class CustomPin : Pin
+    {
+        public string Url { get; set; }
+        public string streetview { get; set; }
+
+    }
+}
---
+++ mymaprender.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MapKit;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Maps.CustomMap), typeof(Maps.iOS.mymaprender))]
+namespace Maps.iOS
+{
+
+    public class mymaprender : Xamarin.Forms.Maps.iOS.MapRenderer
+    {
+        UIView customPinView;
+        List<CustomPin> customPins;
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                var nativeMap = Control as MKMapView;
+                nativeMap.GetViewForAnnotation = null;
+                nativeMap.CalloutAccessoryControlTapped -= OnCalloutAccessoryControlTapped;
+                nativeMap.DidSelectAnnotationView -= OnDidSelectAnnotationView;
+                nativeMap.DidDeselectAnnotationView -= OnDidDeselectAnnotationView;
+            }
+
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                var nativeMap = Control as MKMapView;
+                customPins = formsMap.CustomPins;
+
+                nativeMap.GetViewForAnnotation = GetViewForAnnotation;
+                nativeMap.CalloutAccessoryControlTapped += OnCalloutAccessoryControlTapped;
+                nativeMap.DidSelectAnnotationView += OnDidSelectAnnotationView;
+                nativeMap.DidDeselectAnnotationView += OnDidDeselectAnnotationView;
+            }
+        }
+
+        MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
+        {
+            MKAnnotationView annotationView = null;
+
+            if (annotation is MKUserLocation)
+                return null;
+
+            var customPin = GetCustomPin(annotation as MKPointAnnotation);
+            if (customPin == null)
+            {
+                throw new Exception("Custom pin not found");
+            }
+
+            annotationView = mapView.DequeueReusableAnnotation(customPin.Id.ToString());
+            if (annotationView == null)
+            {
+                annotationView = new CustomMKAnnotationView(annotation, customPin.Id.ToString());
+                annotationView.Image = UIImage.FromFile("pin.png");
+                annotationView.CalloutOffset = new CoreGraphics.CGPoint(0, 0);
+                // annotationView.LeftCalloutAccessoryView = new UIImageView(UIImage.FromFile("monkey.png"));
+                annotationView.RightCalloutAccessoryView = UIButton.FromType(UIButtonType.InfoDark);
+                ((CustomMKAnnotationView)annotationView).Id = customPin.Id.ToString();
+                ((CustomMKAnnotationView)annotationView).Url = customPin.Url;
+            }
+            annotationView.CanShowCallout = true;
+
+            return annotationView;
+        }
+
+        void OnCalloutAccessoryControlTapped(object sender, MKMapViewAccessoryTappedEventArgs e)
+        {
+            var customView = e.View as CustomMKAnnotationView;
+            if (!string.IsNullOrWhiteSpace(customView.Url))
+            {
+                UIApplication.SharedApplication.OpenUrl(new Foundation.NSUrl(customView.Url));
+            }
+        }
+
+        void OnDidSelectAnnotationView(object sender, MKAnnotationViewEventArgs e)
+        {
+            var customView = e.View as CustomMKAnnotationView;
+            customPinView = new UIView();
+
+
+            customPinView.Frame = new CoreGraphics.CGRect(0, 0, 200, 84);
+            var lb = new UILabel(new CoreGraphics.CGRect(0, 0, 200, 84));
+            lb.Text = "StreetView";
+            lb.BackgroundColor = UIColor.White;
+            //     UITapGestureRecognizer tapGesture = new UITapGestureRecognizer(TapThat);
+            // lb.GestureRecognizers=new UIGestureRecognizer()
+            //image.Image = UIImage.FromFile("xamarin.png");
+            customPinView.AddSubview(lb);
+            customPinView.Center = new CoreGraphics.CGPoint(0, -(e.View.Frame.Height + 75));
+            e.View.AddSubview(customPinView);
+
+            //if (customView.Id == "Xamarin")
+            //{
+            //customPinView.Frame = new CoreGraphics.CGRect(0, 0, 200, 84);
+            //var image = new UIImageView(new CoreGraphics.CGRect(0, 0, 200, 84));
+            //image.Image = UIImage.FromFile("xamarin.png");
+            //customPinView.AddSubview(image);
+            //customPinView.Center = new CoreGraphics.CGPoint(0, -(e.View.Frame.Height + 75));
+            //e.View.AddSubview(customPinView);
+            //}
+        }
+
+        void OnDidDeselectAnnotationView(object sender, MKAnnotationViewEventArgs e)
+        {
+            if (!e.View.Selected)
+            {
+                customPinView.RemoveFromSuperview();
+                customPinView.Dispose();
+                customPinView = null;
+            }
+        }
+
+        CustomPin GetCustomPin(MKPointAnnotation annotation)
+        {
+            var position = new Position(annotation.Coordinate.Latitude, annotation.Coordinate.Longitude);
+            foreach (var pin in customPins)
+            {
+                if (pin.Position == position)
+                {
+                    return pin;
+                }
+            }
+            return null;
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+
+
+    public class CustomMKAnnotationView : MKAnnotationView
+    {
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public CustomMKAnnotationView(IMKAnnotation annotation, string id)
+            : base(annotation, id)
+        {
+        }
+    }
+}
